5ef684443a3b3094bc6e4515ce87fac18eb9e3d7
AchBenlakhdar <104871915+AchBenlakhdar@users.noreply.github.com>
2023-12-11 19:04:13 +0000

    ..

---
+++ src/components/sections/studies.js
@@ -0,0 +1,182 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import sr from '@utils/sr';
+import { srConfig, github } from '@config';
+import styled from 'styled-components';
+import { theme, mixins, media, Section, Heading } from '@styles';
+const { colors, fontSizes, fonts } = theme;
+
+const StyledContainer = styled(Section)`
+  position: relative;
+`;
+const StyledFlexContainer = styled.div`
+  ${mixins.flexBetween};
+  align-items: flex-start;
+  ${media.tablet`display: block;`};
+`;
+const StyledContent = styled.div`
+  width: 60%;
+  max-width: 480px;
+  ${media.tablet`width: 100%;`};
+  a {
+    ${mixins.inlineLink};
+  }
+`;
+const SkillsContainer = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(1, minmax(400px, 800px));
+  overflow: hidden;
+  padding: 0;
+  margin: 20px 0 0 0;
+  list-style: none;
+`;
+const Skill = styled.li`
+  position: relative;
+  margin-bottom: 10px;
+  padding-left: 20px;
+  font-family: ${fonts.SFMono};
+  font-size: ${fontSizes.smish};
+  color: ${colors.green};
+  &:before {
+    content: '▹';
+    position: absolute;
+    left: 0;
+    color: ${colors.green};
+    font-size: ${fontSizes.sm};
+    line-height: 12px;
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  width: 40%;
+  max-width: 300px;
+  margin-left: 60px;
+  ${media.tablet`margin: 60px auto 0;`};
+  ${media.phablet`width: 70%;`};
+  a {
+    &:focus {
+      outline: 0;
+    }
+  }
+`;
+const StyledAvatar = styled(Img)`
+  position: relative;
+  mix-blend-mode: multiply;
+  filter: grayscale(100%) contrast(1);
+  border-radius: ${theme.borderRadius};
+  transition: ${theme.transition};
+`;
+const StyledAvatarLink = styled.a`
+  ${mixins.boxShadow};
+  width: 100%;
+  position: relative;
+  border-radius: ${theme.borderRadius};
+  background-color: ${colors.lightestSlate};
+  margin-left: -20px;
+  &:hover,
+  &:focus {
+    background: transparent;
+    &:after {
+      top: 15px;
+      left: 15px;
+    }
+    ${StyledAvatar} {
+      filter: none;
+      mix-blend-mode: normal;
+    }
+  }
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: ${theme.borderRadius};
+    transition: ${theme.transition};
+  }
+  &:before {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: ${colors.navy};
+    mix-blend-mode: screen;
+  }
+  &:after {
+    border: 2px solid ${colors.green};
+    top: 10px;
+    left: 10px;
+    z-index: -1;
+  }
+`;
+
+const Studies = ({ data }) => {
+  const { frontmatter } = data[0].node;
+  const { avatar } = frontmatter;
+  const revealContainer = useRef(null);
+  useEffect(() => sr.reveal(revealContainer.current, srConfig()), []);
+
+  return (
+    <StyledContainer id="about" ref={revealContainer}>
+      <Heading>{'Honors & Awards'}</Heading>
+      <StyledFlexContainer>
+        <StyledContent>
+          {
+            <SkillsContainer>
+              <Skill>
+                {`${'Back-End Development'}: `}
+                <span style={{ color: 'white' }}>
+                  {'Java | Node.js | Python | C++ | Java EE | Spring Boot'}
+                </span>
+              </Skill>
+
+              <Skill>
+                {`${'Front-End Development'}: `}
+                <span style={{ color: 'white' }}>
+                  {'React | Angular | Flutter | Bootstrap | JSP'}
+                </span>
+              </Skill>
+              <Skill>
+                {`${'Database Management'}: `}
+                <span style={{ color: 'white' }}>
+                  {'MySQL | PostgreSQL | MongoDB | Oracle Database | PL/SQL  '}
+                </span>
+              </Skill>
+
+              <Skill>
+                {`${'Data Science and Machine Learning'}: `}
+                <span style={{ color: 'white' }}>
+                  {'Python(Pandas, NumPy) | Scikit-learn | TensorFlow | PyTorch | Keras | OpenCV'}
+                </span>
+              </Skill>
+              <Skill>
+                {`${'Big Data & Distributed Computing'}: `}
+                <span style={{ color: 'white' }}>{'Hadoop | Spark | Redis | Apache Kafka'}</span>
+              </Skill>
+
+              <Skill>
+                {`${'IT Governance & IT Audit'}: `}
+                <span style={{ color: 'white' }}>
+                  {'Knowledge of ITIL | ISO 27001 | Risk Management | ISACA’s IT Audit Standards'}
+                </span>
+              </Skill>
+            </SkillsContainer>
+          }
+        </StyledContent>
+        <StyledPic>
+          <StyledAvatarLink href={github}>
+            <StyledAvatar fluid={avatar.childImageSharp.fluid} alt="Avatar" />
+          </StyledAvatarLink>
+        </StyledPic>
+      </StyledFlexContainer>
+    </StyledContainer>
+  );
+};
+
+Studies.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+export default Studies;
